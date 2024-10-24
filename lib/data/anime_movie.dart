ea85f453af41b86254767ebefc8c41ab84519d89
fadlyoktapriadi <fadlyoktapriadi41@gmail.com>
2024-10-24 19:02:08 +0700

    add data anime

---
+++ lib/data/anime_movie.dart
@@ -0,0 +1,15 @@
+class AnimeMovie {
+  String name;
+  String genre;
+  String rating;
+  String description;
+  String imagePoster;
+ 
+  AnimeMovie({
+    required this.name,
+    required this.genre,
+    required this.rating,
+    required this.description,
+    required this.imagePoster,
+  });
+}
---
+++ lib/data/list_movie_anime.dart
@@ -0,0 +1,75 @@
+import 'package:fynime/data/anime_movie.dart';
+
+var animeMovieList = [
+
+  AnimeMovie(
+    name: "WEATHERING WITH YOU",
+    genre: "Romance, Drama",
+    rating: "⭐⭐⭐⭐⭐ | 5.0",
+    description: "Hodaka merupakan seorang pekerja di sebuah majalah kecil. Sementara, Hina merupakan gadis yang tampak biasa saja tapi ternyata mempunyai kekuatan dalam memunculkan matahari dan mendatangkan hujan. Suatu hari mereka berdua bertemu. Mereka lantas memanfaatkan kemampuan Hina untuk melakukan hal-hal yang mereka mau.",
+    imagePoster: "anime1.jpg"
+  ),
+  AnimeMovie(
+    name: "5 CENTIMETERS PER SECOND",
+    genre: "Drama",
+    rating: "⭐⭐⭐⭐ | 4.2",
+    description: "Dikisahkan, Toono Takaki dan Shinohara Akari dua orang yang sudah lama menjalin hubungan. Suatu hari, mereka harus berpisah secara terpaksa lantaran Akari harus ikut orangtuanya pindah rumah. Meski sudah tinggal terpisah, keduanya tetap menjalin komunikasi lewat berkirim surat. Namun jarak dan kesibukan tetap menjadi masalah yang cukup berarti.",
+    imagePoster: "anime2.jpg"
+  ),
+  AnimeMovie(
+    name: "THE WIND RISES",
+    genre: "Child, War",
+    rating: "⭐⭐⭐⭐ | 4.5",
+    description: "Anime bercerita tentang seorang anak bernama Jirou yang bermimpi bisa menciptakan pesawat terbang. Jirou adalah sosok yang gigih. Pada akhirnya, Jirou berhasil mewujudkan impiannya tersebut. Namun tentunya, perjalanan Jirou untuk mewujudkan impiannya itu tentu tidaklah mudah. Banyak pertentangan yang terjadi dan menempa kehidupan Jirou.",
+    imagePoster: "anime3.jpg"
+  ),
+  AnimeMovie(
+    name: "MIRAI NO MIRAI",
+    genre: "Romance, Drama",
+    rating: "⭐⭐⭐⭐ | 4.7",
+    description: "MIRAI NO MIRA mempunyai cerita yang seru dan menarik. Tokoh dalam anime ini adalah Kun Oota, seorang anak berusia 4 tahun. Kun Oota merupakan anak kesayangan di keluarganya. Namun kehidupan Kun mulai berubah setelah dia mempunyai seorang adik perempuan bernama Mirai. Pasalnya, sejak itu kedua orangtua Kun jadi lebih menyayangi Mirai. Lantaran merasa sedih, Kun akhirnya memutuskan pergi ke sebuah taman. Saat itu, peristiwa ajaib terjadi. Kun bisa bertemu dengan beberapa orang dari masa lalu dan masa depan di kehidupannya, termasuk Mirai dalam usia remaja. Bersama Mirai remaja, Kun akhirnya melakukan sebuah petualangan menakjubkan.",
+    imagePoster: "anime4.jpg"
+  ),
+  AnimeMovie(
+    name: "PONYO ON THE CLIFF BY THE SEA",
+    genre: "Fantasy, Adventure",
+    rating: "⭐⭐⭐⭐⭐ | 5.0",
+    description: "Ponyo bukanlah manusia melainkan seekor ikan. Ia berambisi menjadi seperti manusia. Memiliki kekuatan ajaib, tidak jarang ia mengubah wujudnya. Pertemuan keduanya bermula saat Sosuke menemukan Ponyo saat di pantai. Hubungan keduanya bertambah dekat. Ponyo dan Sosuke juga saling menyukai. Namun, perasaan tersebut memiliki konsekuensi tersendiri.",
+    imagePoster: "anime5.jpg"
+  ),
+  AnimeMovie(
+    name: "I WANT EAT YOUR PANCREAS",
+    genre: "Romance, Drama",
+    rating: "⭐⭐⭐⭐ | 4.4",
+    description: "Kisahnya berfokus pada karakter Sakura Yamauchi. Ia menyembunyikan penyakitnya dari teman-temannya. Namun, suatu hari Haruki, teman sekolah Sakura tidak sengaja mengetahui penyakit pankreas tersebut. Berawal dari sinilah hubungan keduanya menjadi dekat. Di sisi lain, perasaan keduanya semakin berkembang lebih dalam. Akan tetapi, waktu Sakura tidak lama lagi membuat Haruki tak ingin temannya tersebut pergi.",
+    imagePoster: "anime6.jpg"
+  ),
+  AnimeMovie(
+    name: "GINTAMA: THE VERY FINAL",
+    genre: "Fight, Comedy",
+    rating: "⭐⭐⭐⭐ | 4.8",
+    description: "Mengisahkan Gintoki Sakata yang menghadapi musuh paling kuat yakni Utsuro. Kekuatan Utsuro bahkan semakin bertambah. Rupanya, munculnya kembali Utsuro akan mengancam orang-orang terdekat Gintoki. Bersama dengan karakter lainnya, Gintoki berusaha mengalahkan Utsuro.",
+    imagePoster: "anime7.jpg"
+  ),
+  AnimeMovie(
+    name: "SPY×FAMILY CODE: WHITE",
+    genre: "Fight, Comedy",
+    rating: "⭐⭐⭐⭐ | 4.8",
+    description: "Menyoroti kisah Anya bersama keluarga Loid Forger. Awalnya, Loid sedang menyamar menjadi mata-mata. Ia mendapat misi khusus. Keluarga Forger ini kemudian memutuskan pergi ke Frigis untuk belajar memasak. Pasalnya, Anya ingin belajar memasak makanan tradisional demi mendapat hadiah yang menarik. Di perjalanan mereka, kejadian tidak terduga turut terjadi. Anya juga menemukan sesuatu yang menyimpan sebuah rahasia yang mengancam perdamaian. Loid yang tengah menjadi mata-mata dalam operasi Strix turut mengalami tantangan tersendiri.",
+    imagePoster: "anime8.jpg"
+  ),
+  AnimeMovie(
+    name: "BLACK CLOVER: MAHOU TEI NO KEN",
+    genre: "Fight, Fantasy",
+    rating: "⭐⭐⭐⭐ | 4.7",
+    description: "Mengisahkan karakter utama bernama Yuno dan Asta. Tinggal di dunia sihir, Yuno dan Asta sama-sama berambisi menjadi Wizard King. Di sisi lain, Asta rupanya tak bisa menggunakan kemampuan sihirnya. Meski begitu, Asta selalu berusaha berlatih agar kekuatan sihir tersebut berhasil ia miliki. Dalam perjalanannya, rupanya kebangkitan Wizard King sebelumnya menjadi ancaman tersendiri termasuk bagi penduduk kerajaan Semanggi.",
+    imagePoster: "anime9.jpg"
+  ),
+  AnimeMovie(
+    name: "ONE PIECE FILM: RED",
+    genre: "Romance, Drama",
+    rating: "⭐⭐⭐⭐⭐ | 5.0",
+    description: "Mengisahkan Lutfy beserta rombongan yang berlayar menu Pulau Elegia. Tujuannya adalah untuk menyaksikan konser Uta dengan suara merdunya. Tapi ternyata, rombongan Lutfy menemukan fakta mengejutkan bahwa pulau tersebut tidaklah nyata. Belum lagi, Uta ternyata menyembunyikan identitasnya. Agar bisa keluar, ia harus mengalahkan musuh yang muncul. Pada momen tersebut, Shanks kembali hadir.",
+    imagePoster: "anime10.jpg"
+  )
+];
